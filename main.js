46f98ee9ffe500577e7d63830c21e6eb0ac57555
Chemonin <iachemonin@gmail.com>
2020-05-31 11:19:01 +0200

    added file systematization

---
+++ main.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+let srcDir = './src';
+
+const readDir = (source) => {
+    const files = fs.readdirSync(source);
+    let targetDir = path.join(__dirname, './dest');
+    if (!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir);
+    }
+    files.forEach(elem => {
+        let elemPath = path.join(source, elem);
+        let stat = fs.statSync(elemPath);
+        if (!stat.isDirectory()) {
+            let newElementPath = path.join(targetDir, elem[0]);
+            if (!fs.existsSync(path.join(targetDir, elem[0]))) {
+                fs.mkdirSync(path.join(targetDir, elem[0]));
+            }
+            fs.link(path.join(__dirname, elemPath), path.join(newElementPath, elem), err => {
+                if (err) {
+                    console.error(err.message);
+                    return;
+                }
+            });
+        } else {
+            readDir(path.join(source,elem));
+        }
+    })
+}
+
+readDir(srcDir);
