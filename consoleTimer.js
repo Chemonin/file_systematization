4e0c70e71700f2da43e3c5481abcabd178a49162
Chemonin <iachemonin@gmail.com>
2020-06-09 23:20:09 +0200

    refactor async sort module

--- consoleTimer.js
+++ consoleTimer.js
@@ -4,15 +4,24 @@ const app = express();
 process.env.SHOWTIME = 10000;
 process.env.INTERVAL = 2000;
 
+const setTimer = () => {
+    let currentInterval = process.env.SHOWTIME;
+    return new Promise((resolve) => {
+        let intervalId = setInterval(() => {
+            console.log(new Date().toUTCString());
+            if (currentInterval <= 0) {
+                clearInterval(intervalId);
+                resolve(new Date().toUTCString())
+            }
+            currentInterval -= process.env.INTERVAL;
+        }, process.env.INTERVAL)
+    })
+}
 app.get('/', (req, res) => {
-    let intervalId = setInterval(() => {
-        console.log(new Date().toUTCString());
-        process.env.SHOWTIME -= process.env.INTERVAL;
-        if (process.env.SHOWTIME < 0) {
-            clearInterval(intervalId);
-            res.send(new Date().toUTCString())
-        }
-    }, process.env.INTERVAL)
+    setTimer().then((finishTime) => {
+        console.log('finish time');
+        res.send(finishTime)
+    });
 })
 
 app.listen(3000, () => {
